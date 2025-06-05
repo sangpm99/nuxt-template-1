27938c0d0d848355923a58aa1d2cf728b9ec5974
sangpm99 <sangpm72@wru.vn>
2025-06-05 17:41:53 +0700

    Updated

---
+++ components/Gap.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  height?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  height: "30px",
+});
+</script>
+
+<template>
+  <div :style="`height: ${props.height}; width: 100%`"></div>
+</template>
